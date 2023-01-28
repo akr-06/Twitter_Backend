216fc067686c6d2f3c659220d5d35b5381339957
Amit Kumar Roy <akumarroy86@gmail.com>
2023-01-28 16:29:09 +0530

    Added hashtag repository and updated tweet service

---
+++ src/repository/hashtag-respository.js
@@ -0,0 +1,51 @@
+const Hashtag = require('../models/hashtags');
+
+class HashtagRepository {
+
+    async create(){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    async bulkCreate(data){
+        try {
+            const tags = await Hashtag.insertMany(data);
+            return tags;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async get(id){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    async destroy(){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    async findByName(titleList){
+        try {
+            const tags = await Hashtag.find({
+                title : titleList
+            })
+            return tags;
+        } catch (error) {
+            
+        }
+    }
+
+}
+
+module.exports = HashtagRepository;
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,17 +1,40 @@
-const {TweetRepository} = require('../repository/index');
+const {TweetRepository, HashtagRepository} = require('../repository/index');
 
 class TweetService{
 
     constructor(){
         this.tweetRepository = new TweetRepository();
+        this.HashtagRepository = new HashtagRepository();
     }
 
     async create(data){
+
         const content = data.content;
-        const tags = content.match(/#[a-zA-Z0-9_]+/g);
-        tags = tags.map((tag)=>{
+        let tags = content.match(/#[a-zA-Z0-9_]+/g);
+        tags = tags.tolowercase().map((tag)=>{
             return tag.substring(1);
         })
+
+        const tweet = await this.tweetRepository.create(data);
+        let alreadyPresentTags = await this.HashtagRepository.findByName(tags);
+        alreadyPresentTags = alreadyPresentTags.map((tag)=> tag.title);
+
+        let newTags = tags.filter(tag => !alreadyPresentTags.includes(tag));
+        newTags = newTags.map(tag => {
+            return {
+                title : tags,
+                tweets : [tweet.id]
+            }
+        })
+
+        await this.HashtagRepository.bulkCreate(newTags);
+
+        alreadyPresentTags.forEach((tag)=> {
+            tag.tweets.push(tweet.id);
+            tag.save();
+        })
+        
+        return tweet;
     }
 }
 
